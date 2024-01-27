ec91ad671d798f69b5708376c726f3ea46dc1069
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2024-01-27 15:04:47 +0100

    update logout function

--- logout/logout.js
+++ logout/logout.js
@@ -7,9 +7,9 @@ const logout = async (req) => {
     const tokenWithoutBearer = tokenFromRequest.slice(7)
     const userFromToken = jwt.decode(tokenWithoutBearer);
 
-    let user = await User.findOneAndUpdate({ _id: userFromToken.id },{token:null}) ;
+    return  await User.findOneAndUpdate({ _id: userFromToken.id },{token:null}) ;
  
-    return 'Wylogowano'
+   
 }
 
 export {logout}
